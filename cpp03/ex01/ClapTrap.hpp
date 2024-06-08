e1341d325616e1a019907808ba30efd808977ca2
samusanc <samusanc@student.42madrid.com>
2024-06-08 05:11:58 +0200

    uploading the test

---
+++ cpp03/ex01/ClapTrap.hpp
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: samusanc <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/08 19:56:16 by samusanc          #+#    #+#             */
+/*   Updated: 2024/06/08 05:09:03 by samusanc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef	CLAPTRAP_HPP
+# define CLAPTRAP_HPP
+# include <string>
+# include <iostream>
+
+class	ClapTrap{
+	/* here every public statement */
+	public:
+		/* Constructor */
+		ClapTrap(std::string title);
+		/* Copy Constructor */
+		ClapTrap(const ClapTrap& other);
+		/* Copy assignment operator overload */
+		ClapTrap& operator = (const ClapTrap& other);
+		/* Destructor */
+		~ClapTrap(void);
+
+		void attack(const std::string& target);
+		void takeDamage(unsigned int amount);
+		void beRepaired(unsigned int amount);
+	/* here every private statement */
+	private:
+		std::string	name;
+		int		hitPoints;
+		int		energyPoints;
+		int		attackDamage;
+};
+
+#endif
