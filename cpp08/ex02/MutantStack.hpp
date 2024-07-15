c48f51932a186df6cb013084f6b9ed5c6c04f51a
samusanc <samusanc@student.42madrid.com>
2024-07-15 23:52:16 +0200

    uploading cpp

--- cpp08/ex02/MutantStack.hpp
+++ cpp08/ex02/MutantStack.hpp
@@ -6,7 +6,7 @@
 /*   By: samusanc <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/08 19:56:16 by samusanc          #+#    #+#             */
-/*   Updated: 2024/07/12 05:17:46 by samusanc         ###   ########.fr       */
+/*   Updated: 2024/07/15 23:51:50 by samusanc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,76 +15,197 @@
 # include <string>
 # include <iostream>
 
+/*
+template <typename T>
+class	Tlist{
+	public:
+		Tlist<T>(void)
+		{
+			content = 0;
+			next = NULL;
+			last = NULL;
+		}
+
+		Tlist<T>(T con)
+		{
+			content = con;
+			next = NULL;
+		}
+
+		bool	end(void) const
+		{
+			if (next == NULL)
+				return (true);
+			return (false);
+		}
+
+		Tlist	*getNext(void)
+		{
+			return (next);
+		}
+
+		Tlist	*getLast(void)
+		{
+			return (last);
+		}
+
+		T&	getContent(void)
+		{
+			return (content);
+		}
+
+		void	setNext(Tlist *nex)
+		{
+			next = nex;
+		}
+
+		void	setLast(Tlist *las)
+		{
+			last = las;
+		}
+
+	private:
+		class Tlist	*next;
+		class Tlist	*last;
+		T		content;
+};
+*/
+
 template <typename T>
 class	MutantStack{
-	private:
-		struct Node
-		{
-			T data;
-			Node* next;
-			Node(const T& value) : data(value), next(nullptr) {}
-		};
-		
-		Node* head;
-		Node* tail;
+
 	public:
-		MutantStack() : head(nullptr), tail(nullptr) {}
-		
-		~MutantStack() {
-			Node* current = head;
-			while (current != nullptr)
+		/* Constructor */
+		MutantStack<T>(void)
+		{
+			head = NULL;
+			tail = NULL;
+			len = 0;
+		}
+
+		/* Destructor */
+		~MutantStack<T>(void)
+		{
+			Tlist<T>	*tmp;
+
+			tmp = NULL;
+			if (head == NULL)
+				return ;
+			while (head->getNext())
 			{
-				Node* next = current->next;
-				delete current;
-				current = next;
+				tmp = head->getNext();
+				delete head;
+				head = tmp;
 			}
 		}
-	
-		void add(const T& value) {
-			Node* newNode = new Node(value);
-			if (tail)
+
+		unsigned int	size() const
+		{
+			return (len);
+		}
+
+		T&	top()
+		{
+			if (!head)
+				throw	std::out_of_range ("No elements in the stack");
+			return (head->getContent());
+		}
+
+		/* Push Value */
+		void	push(T	content)
+		{
+			Tlist<T>	*tmp;
+
+			tmp = new Tlist<T>(content);
+			if (!tmp)
+				throw	std::out_of_range ("Memory error");
+			if (head)
 			{
-				tail->next = newNode;
-				tail = newNode;
+				tmp->setLast(tail);
+				tail->setNext(tmp);
 			}
 			else
 			{
-				head = tail = newNode;
+				head = tmp;
+				tail = tmp;
 			}
+			tail = tmp;
+			len++;
 		}
-	
-	class iterator {
-		private:
-			Node* current;
-	
-		public:
-			using iterator_category = std::forward_iterator_tag;
-			using value_type = T;
-			using difference_type = std::ptrdiff_t;
-			using pointer = T*;
-			using reference = T&;
-			
-			iterator(Node* node = nullptr) : current(node) {}
-			
-			T& operator*() const { return current->data; }
-			T* operator->() const { return &(current->data); }
-			
-			iterator& operator++() {
-				if (current) current = current->next;
-				return *this;
-			}
-			
-			iterator operator++(int) {
-				iterator temp = *this;
-				++(*this);
-				return temp;
-			}
-			
-			bool operator==(const iterator& other) const { return current == other.current; }
-			bool operator!=(const iterator& other) const { return current != other.current; }
-	};
-	iterator begin() { return iterator(head); }
-	iterator end() { return iterator(nullptr); }
+
+		class	iterator{
+			public:
+				iterator(Tlist<T> *node)
+				{
+					actualNode = node;
+				}
+				
+				T& operator *()
+				{
+					if (actualNode)
+						return(actualNode->getContent());
+					else
+						throw	std::out_of_range ("out of range iterator");
+				}
+
+				MutantStack<T>::iterator	operator ++()
+				{
+					if (actualNode)
+						actualNode = actualNode->getNext();
+					else
+						throw	std::out_of_range ("out of range iterator");
+					return	(MutantStack<T>::iterator(actualNode));
+				}
+
+				MutantStack<T>::iterator	operator ++(int)
+				{
+					Tlist<T>	*tmp = actualNode;
+
+					if (actualNode)
+						actualNode = actualNode->getNext();
+					else
+						throw	std::out_of_range ("out of range iterator");
+					return	(MutantStack<T>::iterator(tmp));
+				}
+
+				MutantStack<T>::iterator	operator --()
+				{
+					if (actualNode)
+						actualNode = actualNode->getLast();
+					else
+						throw	std::out_of_range ("out of range iterator");
+					return	(MutantStack<T>::iterator(actualNode));
+				}
+
+				MutantStack<T>::iterator	operator --(int)
+				{
+					Tlist<T>	*tmp = actualNode;
+
+					if (actualNode)
+						actualNode = actualNode->getLast();
+					else
+						throw	std::out_of_range ("out of range iterator");
+					return	(MutantStack<T>::iterator(tmp));
+				}
+
+			private:
+				Tlist<T>	*actualNode;
+		};
+
+		MutantStack<T>::iterator	begin(void)
+		{
+			return	(MutantStack<T>::iterator(head));
+		}
+
+		MutantStack<T>::iterator	end(void)
+		{
+			return	(MutantStack<T>::iterator(NULL));
+		}
+
+	private:
+		Tlist<T>	*head;
+		Tlist<T>	*tail;
+		unsigned int	len;
 };
 
 #endif
--- cpp08/ex02/main.cpp
+++ cpp08/ex02/main.cpp
@@ -6,16 +6,53 @@
 /*   By: samusanc <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/11 08:36:52 by samusanc          #+#    #+#             */
-/*   Updated: 2024/07/12 05:19:56 by samusanc         ###   ########.fr       */
+/*   Updated: 2024/07/15 23:45:38 by samusanc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string>
 #include <iostream>
+#include <stack>
 #include "MutantStack.hpp"
 
 int	main()
 {
-	std::cout << "hola mundo" << std::endl;
-	return (0);
+	std::cout << "---------------------my own tests----------------" << std::endl;
+	MutantStack<int>		hola;
+
+	hola.push(77);
+	hola.push(88);
+	hola.push(99);
+	MutantStack<int>::iterator	i = hola.begin();
+
+	
+	i++;//88
+	std::cout << *i << std::endl;
+	std::cout << *++i << std::endl;//99
+	std::cout << *i-- << std::endl;
+	std::cout << *i << std::endl;
+	std::cout << *--i << std::endl;
+	std::cout << "--------------------- 42 test main----------------" << std::endl;
+	MutantStack<int> mstack;
+	mstack.push(5);
+	mstack.push(17);
+	std::cout << mstack.top() << std::endl;
+	mstack.pop();
+	std::cout << mstack.size() << std::endl;
+	mstack.push(3);
+	mstack.push(5);
+	mstack.push(737);
+	//[...]
+	mstack.push(0);
+	MutantStack<int>::iterator it = mstack.begin();
+	MutantStack<int>::iterator ite = mstack.end();
+	++it;
+	--it;
+	while (it != ite)
+	{
+		std::cout << *it << std::endl;
+		++it;
+	}
+	std::stack<int> s(mstack);
+	return 0;
 }
