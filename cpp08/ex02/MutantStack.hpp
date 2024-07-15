612610185cba4fa033d76e72a8a535edab989d6d
samusanc <samusanc@student.42madrid.com>
2024-07-16 00:52:01 +0200

    uploading cpp

--- cpp08/ex02/MutantStack.hpp
+++ cpp08/ex02/MutantStack.hpp
@@ -6,7 +6,7 @@
 /*   By: samusanc <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/08 19:56:16 by samusanc          #+#    #+#             */
-/*   Updated: 2024/07/15 23:51:50 by samusanc         ###   ########.fr       */
+/*   Updated: 2024/07/16 00:51:43 by samusanc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,198 +14,56 @@
 # define CLASS_HPP
 # include <string>
 # include <iostream>
-
-/*
-template <typename T>
-class	Tlist{
-	public:
-		Tlist<T>(void)
-		{
-			content = 0;
-			next = NULL;
-			last = NULL;
-		}
-
-		Tlist<T>(T con)
-		{
-			content = con;
-			next = NULL;
-		}
-
-		bool	end(void) const
-		{
-			if (next == NULL)
-				return (true);
-			return (false);
-		}
-
-		Tlist	*getNext(void)
-		{
-			return (next);
-		}
-
-		Tlist	*getLast(void)
-		{
-			return (last);
-		}
-
-		T&	getContent(void)
-		{
-			return (content);
-		}
-
-		void	setNext(Tlist *nex)
-		{
-			next = nex;
-		}
-
-		void	setLast(Tlist *las)
-		{
-			last = las;
-		}
-
-	private:
-		class Tlist	*next;
-		class Tlist	*last;
-		T		content;
-};
-*/
+# include <stack>
+# include <iterator>
 
 template <typename T>
-class	MutantStack{
-
+class	MutantStack : public std::stack<T>{
 	public:
-		/* Constructor */
-		MutantStack<T>(void)
+		class iterator : public std::iterator<std::bidirectional_iterator_tag, T>
 		{
-			head = NULL;
-			tail = NULL;
-			len = 0;
-		}
-
-		/* Destructor */
-		~MutantStack<T>(void)
-		{
-			Tlist<T>	*tmp;
-
-			tmp = NULL;
-			if (head == NULL)
-				return ;
-			while (head->getNext())
+		public:
+			iterator(typename std::deque<T>::iterator it) : _it(it) {}
+			iterator(const iterator& other) : _it(other._it) {}
+	
+			iterator& operator++()
 			{
-				tmp = head->getNext();
-				delete head;
-				head = tmp;
+				++_it;
+				return *this;
 			}
-		}
-
-		unsigned int	size() const
-		{
-			return (len);
-		}
-
-		T&	top()
-		{
-			if (!head)
-				throw	std::out_of_range ("No elements in the stack");
-			return (head->getContent());
-		}
-
-		/* Push Value */
-		void	push(T	content)
-		{
-			Tlist<T>	*tmp;
-
-			tmp = new Tlist<T>(content);
-			if (!tmp)
-				throw	std::out_of_range ("Memory error");
-			if (head)
+	
+			iterator operator++(int)
 			{
-				tmp->setLast(tail);
-				tail->setNext(tmp);
+				iterator tmp = *this;
+				++_it;
+				return tmp;
 			}
-			else
+	
+			iterator& operator--()
 			{
-				head = tmp;
-				tail = tmp;
+				--_it;
+				return *this;
 			}
-			tail = tmp;
-			len++;
-		}
-
-		class	iterator{
-			public:
-				iterator(Tlist<T> *node)
-				{
-					actualNode = node;
-				}
-				
-				T& operator *()
-				{
-					if (actualNode)
-						return(actualNode->getContent());
-					else
-						throw	std::out_of_range ("out of range iterator");
-				}
-
-				MutantStack<T>::iterator	operator ++()
-				{
-					if (actualNode)
-						actualNode = actualNode->getNext();
-					else
-						throw	std::out_of_range ("out of range iterator");
-					return	(MutantStack<T>::iterator(actualNode));
-				}
-
-				MutantStack<T>::iterator	operator ++(int)
-				{
-					Tlist<T>	*tmp = actualNode;
-
-					if (actualNode)
-						actualNode = actualNode->getNext();
-					else
-						throw	std::out_of_range ("out of range iterator");
-					return	(MutantStack<T>::iterator(tmp));
-				}
-
-				MutantStack<T>::iterator	operator --()
-				{
-					if (actualNode)
-						actualNode = actualNode->getLast();
-					else
-						throw	std::out_of_range ("out of range iterator");
-					return	(MutantStack<T>::iterator(actualNode));
-				}
-
-				MutantStack<T>::iterator	operator --(int)
-				{
-					Tlist<T>	*tmp = actualNode;
-
-					if (actualNode)
-						actualNode = actualNode->getLast();
-					else
-						throw	std::out_of_range ("out of range iterator");
-					return	(MutantStack<T>::iterator(tmp));
-				}
-
-			private:
-				Tlist<T>	*actualNode;
+	
+			iterator operator--(int)
+			{
+				iterator tmp = *this;
+				--_it;
+				return tmp;
+			}
+	
+			bool operator==(const iterator& other) const { return _it == other._it; }
+			bool operator!=(const iterator& other) const { return _it != other._it; }
+	
+			T& operator*() { return *_it; }
+			T* operator->() { return &(*_it); }
+	
+		private:
+			typename std::deque<T>::iterator _it;
 		};
-
-		MutantStack<T>::iterator	begin(void)
-		{
-			return	(MutantStack<T>::iterator(head));
-		}
-
-		MutantStack<T>::iterator	end(void)
-		{
-			return	(MutantStack<T>::iterator(NULL));
-		}
-
-	private:
-		Tlist<T>	*head;
-		Tlist<T>	*tail;
-		unsigned int	len;
+	
+		iterator begin() { return iterator(this->c.begin()); }
+		iterator end() { return iterator(this->c.end()); }
 };
 
 #endif
